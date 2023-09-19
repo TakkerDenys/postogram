f374a5f22dd31a334408213193af470bad62617d
TakkerDenys <Denysgendeyoff@gmail.com>
2023-09-19 22:59:14 +0300

    init backend

---
+++ postogram-api/src/main/java/com/api/postogramapi/PostogramApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.postogramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostogramApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostogramApiApplication.class, args);
+    }
+
+}
---
+++ postogram-api/src/test/java/com/api/postogramapi/PostogramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.postogramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostogramApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
